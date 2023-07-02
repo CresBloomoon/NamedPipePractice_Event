2c329959c783e3488d79eb80c99118428887fb90
CresBloomoon <metroidm2421@gmail.com>
2023-07-02 19:45:57 +0900

    プロジェクト ファイルを追加します。

---
+++ NamedPipePractice.Client/NamedPipePractice.Client.cpp
@@ -0,0 +1,70 @@
+﻿#include <windows.h>
+#include <iostream>
+
+#define PIPE_NAME "\\\\192.168.100.100\\pipe\\MyPipe"
+#define BUFFER_SIZE 1024
+
+HANDLE eventHandle;
+
+DWORD WINAPI SendThread(LPVOID lpParam)
+{
+    // イベントの待機
+    WaitForSingleObject(eventHandle, INFINITE);
+
+    // サーバーへの接続
+    HANDLE hPipe = CreateFileA(PIPE_NAME, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (hPipe == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Failed to connect to the server. Error Code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // メッセージの送信
+    const char* message = "Hello, Server!";
+    DWORD bytesWritten;
+    if (!WriteFile(hPipe, message, strlen(message) + 1, &bytesWritten, NULL))
+    {
+        std::cout << "Failed to send message to the server. Error Code: " << GetLastError() << std::endl;
+        CloseHandle(hPipe);
+        return 1;
+    }
+
+    std::cout << "Message sent to the server." << std::endl;
+
+    // リソースの解放
+    CloseHandle(hPipe);
+
+    return 0;
+}
+
+int main()
+{
+    // イベントの作成
+    eventHandle = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if (eventHandle == NULL)
+    {
+        std::cout << "Failed to create event. Error Code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // サーバーへのメッセージ送信を行うスレッドの作成
+    HANDLE hThread = CreateThread(NULL, 0, SendThread, NULL, 0, NULL);
+    if (hThread == NULL)
+    {
+        std::cout << "Failed to create thread. Error Code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // イベントのシグナル
+    std::cout << "Signaling the event." << std::endl;
+    SetEvent(eventHandle);
+
+    // スレッドの終了待機
+    WaitForSingleObject(hThread, INFINITE);
+
+    // リソースの解放
+    CloseHandle(eventHandle);
+    CloseHandle(hThread);
+
+    return 0;
+}
---
+++ NamedPipePractice.Server/NamedPipePractice.Server.cpp
@@ -0,0 +1,63 @@
+﻿#include <windows.h>
+#include <iostream>
+
+#define PIPE_NAME "\\\\.\\pipe\\MyPipe"
+#define BUFFER_SIZE 1024
+
+int main()
+{
+    // パイプの作成
+    HANDLE hPipe = CreateNamedPipeA(
+        PIPE_NAME,
+        PIPE_ACCESS_DUPLEX,
+        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+        PIPE_UNLIMITED_INSTANCES,
+        BUFFER_SIZE,
+        BUFFER_SIZE,
+        0,
+        NULL);
+    if (hPipe == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Failed to create named pipe. Error Code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // クライアントの接続待機
+    std::cout << "Waiting for client connection..." << std::endl;
+    BOOL isConnected = ConnectNamedPipe(hPipe, NULL);
+    if (!isConnected)
+    {
+        std::cout << "Failed to establish client connection. Error Code: " << GetLastError() << std::endl;
+        CloseHandle(hPipe);
+        return 1;
+    }
+
+    std::cout << "Client connected." << std::endl;
+
+    // メッセージの受信
+    char buffer[BUFFER_SIZE];
+    DWORD bytesRead;
+    if (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, NULL))
+    {
+        std::cout << "Received message from client: " << buffer << std::endl;
+
+        // 応答の送信
+        const char* response = "Hello, Client!";
+        DWORD bytesWritten;
+        if (!WriteFile(hPipe, response, strlen(response) + 1, &bytesWritten, NULL))
+        {
+            std::cout << "Failed to send response to client. Error Code: " << GetLastError() << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Failed to receive message from client. Error Code: " << GetLastError() << std::endl;
+    }
+
+    // リソースの解放
+    FlushFileBuffers(hPipe);
+    DisconnectNamedPipe(hPipe);
+    CloseHandle(hPipe);
+
+    return 0;
+}
